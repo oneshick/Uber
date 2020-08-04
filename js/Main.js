f8b65d4f2b399fad3ce6b00f1de613a0805d7573
oneshick <65504119+oneshick@users.noreply.github.com>
2020-08-05 00:20:09 +0500

    uber

---
+++ js/Main.js
@@ -0,0 +1,46 @@
+function testWebP(callback) {
+
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+    if (support == true) {
+        document.querySelector('body').classList.add('webp');
+    } else {
+        document.querySelector('body').classList.add('no-webp');
+    }
+});
+;
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu_item'),
+        hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+});
+
+newFunction();
+
+function newFunction() {
+    item.forEach(link => {
+        link.addEventListener('click', (e) => {
+            indicator(e.target);
+        });
+    });
+}
